8741dbe5789bccfd9d0b487ee3febadb58ae88bd
therealteaparty <142958423+therealteaparty@users.noreply.github.com>
2024-02-08 12:03:14 -0500

    creates files

---
+++ js/date-and-time.js
@@ -0,0 +1,7 @@
+// Adds the current date and time
+let today = new Date();
+let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+let dateTime = date+' '+time;
+
+document.querySelector('#date-and-time').insertAdjacentHTML('afterbegin', dateTime)
